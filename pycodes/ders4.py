6bec0191abf1460561d1e513a4bb28f49d4f61da
fuadcan <fuadcan@gmail.com>
2018-07-11 23:26:19 +0300

    divided two repos. Carried EN here

---
+++ pycodes/ders4.py
@@ -0,0 +1,15 @@
+"""
+isim = 'adnan'
+ayrik = list(isim)
+ayrik = list(reversed(ayrik))
+print(ayrik)
+"""
+"""
+isim = 'adnan'
+ayrik = list(isim) 
+ayrik.sort(reverse=True)
+print(ayrik)
+
+"""
+
+
